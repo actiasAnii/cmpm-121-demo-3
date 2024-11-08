c0225fa1bf32833435bd612631cd986fea82ddc6
actiasAnii <animonte@ucsc.edu>
2024-11-08 00:40:25 -0800

    tiny initial change

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,14 @@
-// todo
+import "./style.css";
+const APP_NAME = "Geocoin Demo";
+document.title = APP_NAME;
+
+// first tiny change
+const button = document.createElement("button");
+button.textContent = "click!";
+button.style.display = "block";
+button.style.margin = "auto";
+
+button.addEventListener("click", () => {
+  alert("you clicked the button!");
+});
+document.body.appendChild(button);
